e75a970c1963e299d655ad5b421c317ed6472646
Marcin <marpk92@gmail.com>
2023-10-30 20:07:03 +0100

    Add chocolate to cart

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
         Product product1 = new Product("Mleko", 2.5);
         Product product2 = new Product("Chleb", 3.0);
         Product product3 = new Product("Jajka", 4.0);
+        Product product4 = new Product("Czekolada", 4.5);
 
         ShoppingCart cart = new ShoppingCart();
         cart.addProduct(product1);
         cart.addProduct(product2);
         cart.addProduct(product3);
+        cart.addProduct(product4);
 
         cart.displayCart();
         cart.checkout();
