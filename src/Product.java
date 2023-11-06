06a6174bb986404715a591f74019500450ea3d6d
Marcin <marpk92@gmail.com>
2023-11-06 17:14:26 +0100

    zmiana podatku 2

--- src/Product.java
+++ src/Product.java
@@ -7,7 +7,7 @@ class Product {
         validatePrice(price);
         this.name = name;
         this.price = price;
-        this.tax = 23;
+        this.tax = 8;
     }
 
     public String getName() {
