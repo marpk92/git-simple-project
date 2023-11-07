f0b53ccae0d4206c9afc6aa95a3b1650a46a863d
Marcin <marpk92@gmail.com>
2023-11-07 16:08:09 +0100

    nowe pliki

---
+++ src/SecondCustomer.java
@@ -0,0 +1,11 @@
+class SecondCustomer {
+    private String name;
+
+    public SecondCustomer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/ThirdCustomer.java
@@ -0,0 +1,11 @@
+class ThirdCustomer {
+    private String name;
+
+    public ThirdCustomer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
