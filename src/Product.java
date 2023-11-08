d6e010f43928f28d6fadb85fede6d0aaced0752c
BeaS84 <szydlowska.beata+wit@gmail.com>
2023-11-08 18:41:33 +0100

    Dodanie obsługi podatku dla produktów i utworzenie klasy

--- src/Product.java
+++ src/Product.java
@@ -1,11 +1,13 @@
 class Product {
     private String name;
     private double price;
+    private double tax;
 
     public Product(String name, double price) {
         validatePrice(price);
         this.name = name;
         this.price = price;
+        this.tax =23;
     }
 
     public String getName() {
