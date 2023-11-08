fe7e6ee1142336cc6a32fc3329dcabc4ad712669
Aleksandra <aleksandratobuch27.03@gmail.com>
2023-11-08 18:26:39 +0100

    dodanie obsługi podatku dla produktu

---
+++ src/CustomerOla.java
@@ -0,0 +1,11 @@
+class CustomerOla {
+    private String name;
+
+    public CustomerOla(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
