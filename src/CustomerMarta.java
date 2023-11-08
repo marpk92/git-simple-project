f1656bf155fa9047163c8e82e8cac46f1d51573d
mkozdroj <martakozdroj@gmail.com>
2023-11-08 18:39:11 +0100

    dodanie nowej klasy

---
+++ src/CustomerMarta.java
@@ -0,0 +1,11 @@
+class CustomerMarta {
+    private String name;
+
+    public CustomerMarta(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
