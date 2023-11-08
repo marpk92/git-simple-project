5fcdb81a1daa0a5e21d292efaa8a5ffc66db3da4
Anna Margery <anna.szewc@gmail.com>
2023-11-08 18:28:09 +0100

    Dodanie obsługi podatku dla produktow

---
+++ src/CustomerMacin.java
@@ -0,0 +1,16 @@
+public class CustomerMacin {
+
+    private String name;
+    private double price;
+
+    public CustomerMacin(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
