0afc6fafb7f973eac2988dc16949916a5b76e19a
MonikaMieleszko <kawka.monika.89@gmail.com>
2023-11-08 18:26:40 +0100

    Dodanie obsługi podatku dla produktów

---
+++ src/CustomerMonika.java
@@ -0,0 +1,11 @@
+class CustomerMonika {
+    private String name;
+
+    public CustomerMonika(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
